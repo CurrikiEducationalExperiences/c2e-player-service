0d527daa897df69ff9b9f3e962adfeec48a7b0a2
Antonio Etayo <antonioetayo@gmail.com>
2023-12-06 12:02:12 -0400

    Validation bugfix

--- src/controllers/CeeLicensesController.js
+++ src/controllers/CeeLicensesController.js
@@ -6,7 +6,7 @@ const apiSecret = process.env.C2E_SERVICES_API_SECRET;
 async function licenses(req, res, next) {
   const { page = 1, limit = 10, query = '' } = req.query;
 
-  if (typeof page !== 'number' || typeof limit !== 'number' || typeof query !== 'string') {
+  if (isNaN(parseInt(page)) || isNaN(parseInt(limit)) || typeof query !== 'string') {
     return res.status(400).send('Invalid parameter type');
   }
 
