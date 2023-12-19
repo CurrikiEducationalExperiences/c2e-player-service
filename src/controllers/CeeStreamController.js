2622b1c34f7fdd642fa0f8c9ce63dde05e5b79b7
Antonio Etayo <antonioetayo@gmail.com>
2023-12-19 11:12:44 -0400

    Error message handling and formatting changes

--- src/controllers/CeeStreamController.js
+++ src/controllers/CeeStreamController.js
@@ -4,7 +4,14 @@ const {PlatformSetting} = require('../models/platformSetting');
 async function stream(req, res, next) {
   var platformSettings = await PlatformSetting.findOne({ where: {lti_client_id: res.locals.token.clientId}});
   if (!platformSettings) {
-    return res.status(400).send('No matching platform settings found.');
+    return res.status(400).send({
+      status: 400,
+      error: "No matching platform settings found",
+      details: {
+        description: "Your LTI authentication information doesn't match any existing platform settings in the C2E player",
+        message: "No matching platform settings found"
+      }
+    });
   }
 
   const { ceeId } = req.query;
@@ -33,8 +40,14 @@ async function stream(req, res, next) {
     res.writeHead(200, headers);
     fileStream.pipe(res);
   } catch (e) {
-    console.log('CeeStreamController Error:', e);
-    res.send({error: 'Failed to stream file'});
+    return res.status(400).send({
+      status: 400,
+      error: "Failed to stream file",
+      details: {
+        description: "Could not stream C2E content. Please check your licensee settings and query params",
+        message: "Failed to stream file"
+      }
+    });
   }
 }
 
